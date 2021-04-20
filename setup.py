060c9706b1cad266a8225c411d8969f4370e243c
basquiat <iosgino@hotmail.com>
2021-04-20 19:13:23 +0200

    Replaced pyproject.toml with setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,33 @@
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup()
+    setup(
+        name="penitus",
+        version="1.0.0",
+        description="Shows information about cryptocurrencies, like prices.",
+        license="MIT",
+        author="Gino Zanella",
+        author_email="iosgino@hotmail.com",
+        readme="README.md",
+        repository="https://github.com/fckvrbd/penitus",
+        python="^3.0",
+        keywords=[
+            "Cryptocurrency",
+            "Finances",
+            "Bitcoin",
+            "Coinpaprika",
+            "Investing"
+        ],
+        packages=["src/penitus"],
+        classifiers=[
+            "License :: OSI Approved :: MIT License",
+            "Development Status :: 4 - Beta",
+            "Natural Language :: English",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.7",
+            "Topic :: Software Development",
+            "Topic :: Office/Business :: Financial :: Investment",
+        ],
+        install_requires=["requests"]
+    )
